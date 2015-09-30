69170d502e67497d614989592a9f4408c397c0af
vinilzen <marchukilya@gmail.com>
2015-10-01 01:49:39 +0200

    fixs

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
-	$('a.scrollmy[href^="#"], a.scrollmy[href^="."]').click( function(){ // если в href начинается с # или ., то ловим клик
+	$('a.scrollmy[href^="#"]').click( function(){ // если в href начинается с # или ., то ловим клик
 		var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
 		if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
 			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 700); // анимируем скроолинг к элементу scroll_el
 		}
 		return false; // выключаем стандартное действие
 	});
-
-	/*$(document.body).on('appear', '#Advantages', function(e, $affected) {
-		console.log(123);
-	});*/
 
 	$('#Features').appear().on('appear', function(event, $elements) {
 		$('#Features .container-features').css({'opacity':1});
@@ -19,9 +15,14 @@ $(document).ready(function(){
 		$('#Features .container-features').css({'opacity':0.1});
     });
 
-    $('.cover-image').appear().on('disappear', function(event, $all_disappeared_elements) {
-     	console.log('cover-image');
-    });
-
 	$('#myTab').tabCollapse();
+
+	$(window).scroll(function() {
+		// Scroll the background at var speed
+		// the yPos is a negative value because we're scrolling it UP!
+		var yPos = -($(window).scrollTop() / 5); 
+		// Put together our final background position
+		// Move the background
+		$('.section-advantage').css({ backgroundPositionY: yPos + 'px' });
+	});
 });
